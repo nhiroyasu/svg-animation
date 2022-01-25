23b082e974339999ef8a64a0eb60c72b01ac0952
nhiroyasu <namid224y@gmail.com>
2022-01-25 23:08:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vnprft
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vnprft)
---
+++ index.js
@@ -0,0 +1,64 @@
+// Import stylesheets
+import './style.css';
+import anime from 'animejs';
+
+// path strokeのアニメーション
+anime({
+  targets: 'path, circle, rect',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  direction: 'alternate',
+  loop: true,
+});
+
+// rectのアニメーション
+anime({
+  targets: '#グループ_1 > rect',
+  width: function (target) {
+    return [0, target.width.baseVal.value];
+  },
+  height: function (target) {
+    return [0, target.height.baseVal.value];
+  },
+  easing: 'easeInOutSine',
+  duration: 1500,
+  direction: 'alternate',
+  loop: true,
+});
+
+anime({
+  targets: '#sample_4 > rect',
+  width: function (target) {
+    return [0, target.width.baseVal.value];
+  },
+  height: function (target) {
+    return [0, target.height.baseVal.value];
+  },
+  easing: 'easeInOutSine',
+  duration: 1500,
+  direction: 'alternate',
+  loop: true,
+});
+
+// circleのアニメーション
+anime({
+  targets: '#sample_4 > circle',
+  r: function (target) {
+    return [0, target.r.baseVal.value];
+  },
+  easing: 'easeInOutSine',
+  duration: 1500,
+  direction: 'alternate',
+  loop: true,
+});
+
+// path d のアニメーション
+anime({
+  targets: '#sample_4 > path',
+  d: 'M18.5,0,37,32H0Z',
+  easing: 'easeInOutSine',
+  duration: 1500,
+  direction: 'alternate',
+  loop: true,
+});
